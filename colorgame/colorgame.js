b47c778a39b3a77782cbdc9834821f7542550c1c
Bilegsaikhan Bayarkhuu <23LP5355@G401-CC0090.local>
2023-12-21 15:39:01 +0800

    DOM

--- colorgame/colorgame.js
+++ colorgame/colorgame.js
@@ -1,25 +1,79 @@
 const root = document.getElementById("root");
+const header = document.createElement("div");
+header.setAttribute("class", "header");
+
+let levelC = 0;
+const level = document.createElement("p");
+level.innerText = "level " + levelC;
 
 const startButton = document.createElement("button");
+startButton.setAttribute("id", "startButton");
 startButton.innerText = "Start";
-root.appendChild(startButton);
+
+let chanceC = 3;
+
+const chance = document.createElement("p");
+chance.setAttribute("class", "chance");
+chance.innerText = "Chance " + chanceC;
+
+header.appendChild(level);
+header.appendChild(startButton);
+header.appendChild(chance);
+
+const gameBody = document.createElement("div");
+gameBody.setAttribute("class", "gameBody");
+const boxes = document.createElement("div");
+boxes.setAttribute("class", "boxes");
+
+root.appendChild(header);
+root.appendChild(gameBody);
 
 startButton.addEventListener("click", startGame);
 
-function startGame() {
-  const box = document.createElement("div");
-  const diffbox = document.createElement("div");
-  const red = Math.floor(Math.random() * 245) + 10;
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-  const randomColor = `rgb(${red},${green},${blue})`;
-  const differentColor = `rgb(${red - 10},${green},${blue})`;
+function addBoxes() {
+  const red = Math.floor(Math.random() * 250);
+  const green = Math.floor(Math.random() * 250);
+  const blue = Math.floor(Math.random() * 250);
+  const randomColor = `rgb(${red}, ${green},${blue}`;
+  const random = Math.floor(Math.random() * 8);
+  for (i = 0; i < 9; i++) {
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    box.setAttribute("id", i);
+    if (i == random) {
+      box.style.backgroundColor = `rgb(${red-5}, ${green - 5}, ${blue-5})`;
+    } else {
+      box.style.backgroundColor = randomColor;
+    }
+    boxes.appendChild(box);
 
-  box.setAttribute("class", "box");
-  diffbox.setAttribute("class", "box");
-  box.style.backgroundColor = randomColor;
-  root.appendChild(box);
-  root.appendChild(diffbox);
+    box.addEventListener("click", (e) => {
+      let boxId = e.target.id;
+      if (boxId == random) {
+        levelC = levelC + 1;
+        boxes.innerHTML = "";
+        level.innerText = "Level " + levelC;
+        if (levelC == 5) {
+          gameBody.innerHTML = "Win";
+        } else {
+          addBoxes();
+        }
+      } else {
+        chanceC--;
+        if (chanceC == 0) {
+          gameBody.innerHTML = "Lose";
+        }
+        chance.innerText = "Chance " + chanceC;
+      }
+    });
+  }
+  gameBody.appendChild(boxes);
 }
-localStorage.setItem("item", "andy");
-console.log(localStorage.getItem("item"));
+function startGame() {
+  levelC = 0;
+  level.innerText = "Level " + levelC;
+  chanceC = 3;
+  chance.innerText = "Chance " + chanceC;
+  boxes.innerHTML = "";
+  addBoxes();
+}
